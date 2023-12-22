53488b0a5e756fda7c7b545f8b4d4f24caf83752
Pietro Pustina <39011004+piepustina@users.noreply.github.com>
2023-12-22 20:24:01 +0100

    Added src directory

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,8 @@
 import pathlib
 import sys
 import os
-sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+#sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+sys.path.insert(0, "../src")
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -20,12 +21,12 @@ release = '1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+    'sphinxcontrib.matlab',
     'sphinx.ext.duration',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
     'sphinx.ext.imgmath',
-    'sphinxcontrib.matlab',
     'sphinxcontrib.bibtex',
     'sphinx.ext.githubpages',
 ]
@@ -36,7 +37,7 @@ exclude_patterns = []
 
 # Configure the matlab extension
 primary_domain = "mat"
-matlab_src_dir = "../"
+matlab_src_dir = "../src"
 matlab_short_links = True
 
 # Configure the bibtex extension
